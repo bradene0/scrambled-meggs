cedb7e5eab51d1abe95f55e0614d2c3aa933d51a
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:26:12 -0500

    Set interval to run code every second

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
+   setInterval(() => {
     event.target.innerText = event.target.innerText.split("")
     .map(letter => letters[Math.floor(Math.random() * 26)])
     .join('');
+   }, 1000);
 }

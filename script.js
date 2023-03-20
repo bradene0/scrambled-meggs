c201d2ac3ec3b1562aaef57ef15b535d5bbca521
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:42:00 -0500

    Set iterations to length of str

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.querySelector("h1").onmouseover = event => {
     return letters[Math.floor(Math.random() * 26)]
     })
     .join('');
-    if(iterations >= 9) clearInterval(interval);
-    iterations += 1;
+    if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+    iterations += 1 / 4;
    }, 33);
 }

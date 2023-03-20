69120a5076a53e9976817b49eddae8f1e5d5cac1
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:27:36 -0500

    Changed interval to 33 times per second

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.querySelector("h1").onmouseover = event => {
     event.target.innerText = event.target.innerText.split("")
     .map(letter => letters[Math.floor(Math.random() * 26)])
     .join('');
-   }, 100);
+   }, 33);
 }

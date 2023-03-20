6ddded7b347b285e01e901e17f2a838e26c2db50
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:23:50 -0500

    Assigned random letters and joined back together

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = event.target.innerText.split("") letters[Math.floor(Math.random() * 26)];
+    event.target.innerText = event.target.innerText.split("")
+    .map(letter => letters[Math.floor(Math.random() * 26)])
+    .join('');
 }

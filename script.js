e3538f3ccd13e18d54a2ba8308f18d51af350eb5
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:20:31 -0500

    Set range 0-26 instead of previous 0-24

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = letters[Math.floor(Math.random() * 24)];
+    event.target.innerText = letters[Math.floor(Math.random() * 26)];
 }

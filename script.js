73400e3cf197fb00bd6c7dba1bc19bb4bf283cd8
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:30:38 -0500

    Set iteration count to ten (stops at 10)

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.querySelector("h1").onmouseover = event => {
     event.target.innerText = event.target.innerText.split("")
     .map(letter => letters[Math.floor(Math.random() * 26)])
     .join('');
+    if(iterations >= 9) clearInterval(interval);
     iterations += 1;
    }, 33);
 }

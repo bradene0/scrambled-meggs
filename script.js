dc201fdff8152fdad21880ab924635e523abe360
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:21:53 -0500

    Split word

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = letters[Math.floor(Math.random() * 26)];
+    event.target.innerText = event.target.innerText.split("") letters[Math.floor(Math.random() * 26)];
 }

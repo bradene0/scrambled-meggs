8c02cd3038a41d1d64b1d8ce806dcf910d9fa481
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 14:29:42 -0500

    Declared iterations and called

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
+   let iterations = 0;
 
    const interval = setInterval(() => {
     event.target.innerText = event.target.innerText.split("")
     .map(letter => letters[Math.floor(Math.random() * 26)])
     .join('');
+    iterations += 1;
    }, 33);
 }
